85d9205d0eb86628530e3be147ab0fd158632641
anavarro <aldo.navarro@simfoods.com>
2025-09-09 16:29:54 -0500

    basic text entry

--- hw-1/counting-weekends.py
+++ hw-1/counting-weekends.py
@@ -1,1 +1,16 @@
 # this is a python file, be patient!
+import datetime
+
+def main():
+    print("HW1 - Counting Weekends\nSolution by Aldo Navarro\n")
+    while True:
+        try:
+            user_input = input("Enter a number, or Q to quit > ")
+            if user_input.lower() == "q":
+                break        
+            print(user_input)
+        except Exception as e:
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
