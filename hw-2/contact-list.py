55c720f8907fd6a45cad737f7dfafffd18f21255
anavarro <aldo.navarro@simfoods.com>
2025-09-16 15:10:26 -0500

    improved deletion logic and language

--- hw-2/contact-list.py
+++ hw-2/contact-list.py
@@ -25,10 +25,21 @@ class ContactBook():
 
     contact_book = [] # list of lists to store all contacts
 
+    def sortBook(self):
+        self.contact_book.sort()
+        return
+
+    def searchContactbyIndex(self, input): 
+        # Try to find a contact by index, outputs the contact if it is found, None if not 
+        try:
+            output = self.contact_book[input]
+        except IndexError as e: return None
+        return output
+
     def insertContact(self, input):
         try:
             self.contact_book.append(input)
-        except Exception as e: print(f"Failed to add contact: {e}")
+        except Exception as e: print(f"!! Failed to add contact: {e}")
         return 
     
     def removeContactbyName(self, first, last):
@@ -36,9 +47,14 @@ class ContactBook():
         index = next((i for i, r in enumerate(self.contact_book) if first in r and last in r), -1)
         try:
             del self.contact_book[index]
-        except IndexError as e: print(f"Error removing contact: {e}")
-        return
+            return True
+        except IndexError as e: print(f"!! Error deleting contact: {e}"); return False
     
+    def removeContactbyIndex(self, pos:int):
+        try:
+            del self.contact_book[pos]
+            return True
+        except IndexError as e: print(f"!! Error deleting contact: {e}"); return False
 
 def main():
     print("\nHW2 - Contact List\nSolution by Aldo Navarro\n")
@@ -58,41 +74,60 @@ def main():
 
             if user_input == 1: # New Contact
                 t_flag = True
-                in_fname = input("Enter the user's first name > ")
-                in_lname = input("Enter the user's last name > ")
+                in_fname = input(" - Enter the user's first name > ")
+                in_lname = input(" - Enter the user's last name > ")
                 while t_flag:
                     try:
-                        in_bday = input("Enter the user's birthday > ")
+                        in_bday = input(" - Enter the user's birthday > ")
                         selected_contact = Contact(in_fname, in_lname, in_bday) # prepare the contact
                         book.insertContact(selected_contact._contact) # try to insert just the list into the book
                         print(selected_contact, selected_contact._contact) # print the list
                         t_flag = False # break from the loop
-                    except Exception as e: print(f"Error: {e}")
+                    except Exception as e: print(f"!! Error: {e}")
 
             if user_input == 2: # Import Contacts
                 print("WIP")
 
             if user_input == 3: # Display all Contacts
+                book.sortBook()
                 print(book.contact_book)
             
             if user_input == 4: # Export all Contacts
                 print("WIP") 
             
             if user_input == 5: # Delete a Contact
-                user_input = input("Remove contact by [N]ame, [P]osition, or [E]xit > ")
-                if user_input[0].upper() == "N":
-                    in_fname = input("Enter the user's first name > ")
-                    in_lname = input("Enter the user's last name > ")
-                    book.removeContactbyName(in_fname, in_lname)
-                if user_input[0].upper() == "P":
-                    in_pos = input("Enter the contact's position in the list > ")
-                
+                user_input = input(" - Remove contact by [N]ame, [P]osition, or [E]xit > ")
+                if user_input[0].upper() == "N": # by name
+                    in_fname = input(" - - Enter the user's first name > ")
+                    in_lname = input(" - - Enter the user's last name > ")
+                    val = book.removeContactbyName(in_fname, in_lname)
+                    if val == True:
+                        print(f" - Deleted the contact {in_fname.capitalize()} {in_lname.capitalize()}.\n")
+                    else:
+                        print(f"!! Failed to delete the contact, please try again.\n")
 
+                if user_input[0].upper() == "P": # by position in the list
+                    in_pos = input(" - - Enter the contact's position in the list > ")
+                    try:
+                        in_pos = int(in_pos)
+                    except ValueError as e: print(f"! This is not a number.")
+                    x = book.searchContactbyIndex(in_pos)
+                    if x != None:
+                        user_input = input(f" - - Are you sure you want to remove the contact {x[1], x[0]}? y/N > ")
+                        if user_input[0].upper() == "Y":
+                            val = book.removeContactbyIndex(in_pos)
+                            if val == True:
+                                print(f" - Deleted the contact in position {in_pos}.\n")
+                            else:
+                                print(f"!! Failed to delete the contact, please try again.\n")
+                        else: print(" - Did not delete the contact.")
+                    else: print("!! No contact in that position, please verify and try again.")
+                    
             if user_input == 6: # Exit
                 print("HW2 Complete")
                 break
 
-        except Exception as e: print(f"Error: {e}")
+        except Exception as e: print(f"!!! Error: {e}")
 
 if __name__ == "__main__":
     main()
